79228eccb8e4d047319e3f09c15a40df5f977890
strangeratyourdoor <cawuor54@gmail.com>
2023-09-28 20:35:56 +0300

    New file

---
+++ intro.py
@@ -0,0 +1,13 @@
+# simple calculator
+name = input("Enter name: ")
+print(f"Hello, {name}")
+num1 = input("Enter num 1: ")
+num2 = input("Enter num 2: ")
+sum = int(num1) + int(num2)
+print(f"Sum of the two numbers= {sum}")
+
+product = int(num1) * int(num2)
+print(f"And their product is: {product}")
+rem = int(num1) % int(num2)
+print(f"The remainder when num1 is divided by num2 is: {rem}")
+
