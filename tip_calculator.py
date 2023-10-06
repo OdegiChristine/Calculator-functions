a4b60c93372e19788bb96dfc82f5e4d1b706f880
strangeratyourdoor <cawuor54@gmail.com>
2023-10-06 11:23:06 +0300

    Assignment 2

---
+++ tip_calculator.py
@@ -0,0 +1,22 @@
+bill = int(input("Enter bill: "))
+customers = int(input("Enter number of people splitting the bill: "))
+
+def tip_amount(amount : int):
+    if amount <= 2000:
+        tip = amount * 0.01
+
+    elif amount <= 7000:
+        tip = amount * 0.12
+
+    elif amount > 7000:
+        tip = amount * 0.15
+
+    return tip
+
+amount_to_tip = tip_amount(bill)
+total_bill = bill + amount_to_tip
+
+split_bill = total_bill/customers
+rounded_bill = round(split_bill, 2)
+
+print(f"Bill to be paid by each person = {rounded_bill}")
