a9b261ada57395b820483a055b6e3c74c9bc824f
strangeratyourdoor <cawuor54@gmail.com>
2023-10-06 11:21:31 +0300

    Assignment 2

---
+++ calc2.py
@@ -0,0 +1,26 @@
+name = input("Enter name: ")
+print(f"Hello, {name}")
+
+year_of_birth = int(input("Enter year of birth: "))
+current_year = 2023
+
+def age_in_years(yob: int):
+    age_years = current_year - yob
+    print(f"Age in years = {age_years}")
+    return age_years
+
+years = age_in_years(year_of_birth)
+
+def age_in_months(yrs: int):
+    age = yrs * 12
+    print(f"Age in months = {age}")
+    return age
+
+months = age_in_months(years)
+
+def age_in_days(num : int):
+    days = num * 365
+    print(f"Age in days: {days}")
+    return days
+
+age_days = age_in_days(years)
